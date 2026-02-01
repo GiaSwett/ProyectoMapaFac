60a6ad11f31bc37bee5e5ee1bbb9850a215642b7
Itati Huayamave Swett <itatiswett@gmail.com>
2026-02-01 12:31:49 -0500

    Agrego cambios al proyecto

---
+++ js/mapadep.js
@@ -0,0 +1,10 @@
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+
+fetch(`http://localhost:3000/departamento/${id}`)
+  .then(res => res.json())
+  .then(dep => {
+      document.getElementById("titulo").innerText = dep.Nombre;
+      document.getElementById("mapa").src = dep.imagen_mapa;
+  })
+  .catch(err => console.log(err));
